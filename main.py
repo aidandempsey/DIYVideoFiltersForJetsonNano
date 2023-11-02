1010a7e487c5b428f719eae6be396f1994073cc7
Sean Breen <s.breen14@nuigalway.ie>
2023-11-02 20:55:48 +0000

    Sean B initial setup commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def backgroundBlur():
+    #stub code
+    True
+
+def backgroundReplacement():
+    #stub code
+    True
+
+def faceDistortion():
+    #stub code
+    True
+
+def faceFilter():
+    #stub code
+    True
+
+def ourIdea():
+    #stub code
+    True
+
+if __name__ == '__main__':
+    #do stuff
+    print('Aidan and Sean are sound men')
